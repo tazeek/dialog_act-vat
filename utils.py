e7b5ee6644b41b9e833ae2aaf923526cc5086799
tazeek <tazeek.rakib@gmail.com>
2022-11-08 16:52:42 +0800

    Add preprocessing function

--- utils.py
+++ utils.py
@@ -1,16 +1,3 @@
-class TextPreprocess:
+def preprocess_text(utterances: list) -> list:
 
-    def __init__(self):
-
-        # Define the methods used
-        self._methods = {
-            'word2vec': _process_word_vectors()
-        }
-
-    def process_text(self, method: str):
-
-        return self._methods[method]
-
-    def _process_word_vectors(self):
-
-        ...
+    ...
