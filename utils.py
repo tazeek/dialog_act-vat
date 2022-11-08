2e7e44556cb8e84dd2daa79554786af48e1180ab
tazeek <tazeek.rakib@gmail.com>
2022-11-08 16:54:47 +0800

    Split on whitespace

--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
 def preprocess_text(utterances: list) -> list:
 
-    ...
+    # Split on whitespace
+    processed_utterances = [[utterance.split(' ')] for utterance in utterances]
+
+    return processed_utterances
