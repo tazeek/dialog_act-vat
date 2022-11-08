67840f963736353cbd0e860369b564af66a98d16
tazeek <tazeek.rakib@gmail.com>
2022-11-08 16:57:40 +0800

    Process text

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from label_loader import LabelLoader
+from utils import preprocess_text
 
 if __name__ == '__main__':
 
@@ -6,9 +7,12 @@ if __name__ == '__main__':
     label_loader = LabelLoader().fetch_dataframe()
 
     # Differentiate labels and text
-    x_train, y_train = label_loader['dialog_act'], label_loader['utterance']
+    x_train, y_train = label_loader['utterance'], label_loader['dialog_act']
 
+    print(x_train[0])
     # Preprocess the data (DailyDialog)
+    x_train = preprocess_text(x_train)
+    print(x_train)
 
     # Prepare the model
 
