e2f48e4ddc59f7fe68b97cd3b12a0b69550f6abb
tazeek <tazeek.rakib@gmail.com>
2022-11-08 14:58:32 +0800

    DailyDialog loader added

---
+++ label_loader.py
@@ -0,0 +1,13 @@
+class LabelLoader:
+
+    def __init__(self):
+
+        # Directory attribute
+        self._data_dir = 'data\\'
+
+        # Load dialogues
+        # One dialogue: many utterances
+        
+        # Load act annotations
+
+        # Perform mapping
