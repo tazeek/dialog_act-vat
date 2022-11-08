065e9ee667261dadd04c1d84667a3189581d3213
tazeek <tazeek.rakib@gmail.com>
2022-11-08 15:16:27 +0800

    Separation done

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from label_loader import LabelLoader
 if __name__ == '__main__':
 
     # Load the data (DailyDialog)
-     label_loader = LabelLoader().fetch_dataframe()
+    label_loader = LabelLoader().fetch_dataframe()
 
     # Differentiate labels and text
+    x_train, y_train = label_loader['dialog_act'], label_loader['utterance']
 
     # Preprocess the data (DailyDialog)
 
