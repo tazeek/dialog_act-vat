f0c37d58e2e0d0ff1de1edd0bd01ad4ae64d4a33
tazeek <tazeek.rakib@gmail.com>
2022-11-10 15:55:50 +0800

    LSTM attributes defined

--- lstm_glove.py
+++ lstm_glove.py
@@ -2,4 +2,19 @@ from torch import nn
 
 class LSTM_GLove(nn.Module):
 
-    ...
+    def __init__(self, embedding_layer):
+
+        self._batch_size
+
+        self._output_size
+        self._layers
+        self._hidden_nodes
+
+        self._vocab_size
+        self._embedding_dim
+        
+        self._word_embeddings
+        self._word_embeddings.weight
+
+        self._lstm
+        self._label
