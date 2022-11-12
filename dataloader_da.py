7b9ad84e5aee87deb465e0c7d282ab3b672e6482
tazeek <tazeek.rakib@gmail.com>
2022-11-13 02:23:03 +0800

    Dataset class implemented

--- dataloader_da.py
+++ dataloader_da.py
@@ -2,10 +2,19 @@ from torch.utils.data import Dataset
 
 class DataLoader_DA(Dataset):
     def __init__(self, text, labels):
-        ...
+        
+        self._text = text
+        self._labels = labels
     
     def __len__(self):
-        ...
+        return len(self._labels)
 
     def __getitem__(self, idx):
-        ...
+        
+        label = self._labels[idx]
+        text = self.text[idx]
+
+        return {
+            'text': text,
+            'class': label
+        }
