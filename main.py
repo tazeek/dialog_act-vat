ac5530977dba6b42c603bf1a69d0633a945470dd
tazeek <tazeek.rakib@gmail.com>
2022-11-13 21:57:58 +0800

    TODO reordered with VAT at the last

--- main.py
+++ main.py
@@ -12,34 +12,40 @@ if __name__ == '__main__':
     # Differentiate labels and text
     full_file, full_labels = label_loader['utterance'], label_loader['dialog_act']
 
-    # Preprocess the text and get tokens (DailyDialog)
-    #full_file = utils.preprocess_text(full_file, remove_punctuation=False)
+    # Preprocess the text and get tokens for dictionary creation
+    all_possible_words_list = utils.preprocess_text(full_file, remove_punctuation=False)
 
     # Get word to index dictionary
-    #word_to_index = utils.convert_word_index(full_file)
-    #print(f"Number of unique words: {len(word_to_index)}")
-
-    # Transform the text to index format
-    #full_file = utils.transform_text_integer(full_file, word_to_index)
+    word_to_index = utils.convert_word_index(all_possible_words_list)
+    print(f"Number of unique words: {len(word_to_index)}")
 
     # Load GloVe model
-    #glove_model = utils.load_glove_model()
-    #print(f"Length of GloVe model: {len(glove_model)}")
+    glove_model = utils.load_glove_model()
+    print(f"Length of GloVe model: {len(glove_model)}")
 
     # Create lookup table and check for embeddings test
-    #glove_embeddings = utils.create_glove_embeddings(glove_model, word_to_index)
+    glove_embeddings = utils.create_glove_embeddings(glove_model, word_to_index)
     #utils.embeddings_test(glove_embeddings, glove_model, word_to_index, 'hungry')
 
     # Break up the dataset into training and testing
     # TODO: use the zip files to load the information
     x_train, y_train, x_test, y_test = utils.split_training_testing(full_file, full_labels)
 
+    # Preprocessing steps
+    x_train = utils.preprocess_text(x_train, remove_punctuation=False)
+    x_test = utils.preprocess_text(x_test, remove_punctuation=False)
+
+    # Transform to integer format for lookup
+    x_train = utils.transform_text_integer(x_train, word_to_index)
+    x_test = utils.transform_text_integer(x_test, word_to_index)
+
     # Use Dataset class for PyTorch
     # and use dataloader to load
-    #train_set = DataLoader_DA(full_file, full_labels)
-    #train_generator = utils.transform_dataloader(train_set)
+    train_set = DataLoader_DA(x_train, y_train)
+    train_generator = utils.transform_dataloader(train_set)
 
-    # Create the VAT formula
+    test_set = DataLoader_DA(x_test, y_test)
+    test_generator = utils.transform_dataloader(test_set)
 
     # Prepare the model
     #model = LSTM_GLove(glove_embeddings)
@@ -48,3 +54,5 @@ if __name__ == '__main__':
     #utils.train_model(train_generator, glove_embeddings)
 
     # Test the model
+
+    # Create the VAT formula
