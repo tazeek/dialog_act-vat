dc4eb86d00432caf119dcd18e56bd8b81580d79c
tazeek <tazeek.rakib@gmail.com>
2022-11-16 16:12:28 +0800

    Extract the text and label files

--- dailydialog_loader.py
+++ dailydialog_loader.py
@@ -1,19 +1,30 @@
-
+import zipfile
 
 class DailyDialog_Loader():
 
     def __init__(self, filename):
 
-        self._filename = 'label_data/' + filename
+        self._filename = 'label_data\\' + f'\{filename}'
 
         # Open Zip file
-
-        # Extract the text
-
-        # Extract the labels
+        self._open_zip()
 
     def _open_zip(self):
 
+        with zipfile.ZipFile(self._filename) as z:
+            
+            files = z.namelist()
+
+            # Get the text and label file name
+            text_file, label_file = files[3], files[1]
+
+            # Extract the text
+            print(text_file)
+            print(label_file)
+
+            # Extract the labels
+
+        return None
         ...
 
     def _extract_dialog(self):
