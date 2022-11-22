60aa9004679b93f9dec2580b0e69f9581a4fe303
tazeek <tazeek.rakib@gmail.com>
2022-11-22 16:17:49 +0800

    Move metrics evaluations as well

--- train_model.py
+++ train_model.py
@@ -1,6 +1,9 @@
 from models import lstm_glove
 from vat_loss import VATLoss
+from torchmetrics import Accuracy
+from torchmetrics.classification import MulticlassF1Score
 
+import pandas as pd
 import torch
 
 class Model():
@@ -17,6 +20,34 @@ class Model():
 
         # Device
         self._device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    def metrics_evaluation(y_pred, y_train, device):
+
+        # Get the predicted labels
+        y_pred_softmax = torch.log_softmax(y_pred, dim = 1)
+        _, y_pred_tags = torch.max(y_pred_softmax, dim = 1)
+
+        accuracy = Accuracy().to(device)
+        f1 = MulticlassF1Score(num_classes = 4).to(device)
+
+        acc_score = accuracy(y_pred_tags, y_train)
+        acc_score = torch.round(acc_score * 100)
+
+        f1_score = f1(y_pred_tags, y_train)
+        f1_score = torch.round(f1_score * 100)
+
+        return acc_score, f1_score
+
+    def _save_csv_file(self, data, base_file) -> None:
+
+        # Convert to dataframe
+        df = pd.DataFrame(data)
+
+        # Save dataframe to CSV
+        file_name = 'results/' + base_file + '_training_results.csv'
+        df.to_csv(file_name, index=False)
+
+        return None
 
     def _get_results_dictionary(self):
 
