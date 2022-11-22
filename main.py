165b11061b6e57a773ef187ec9fd01ba58d84593
tazeek <tazeek.rakib@gmail.com>
2022-11-22 15:51:10 +0800

    Move parser and logger to util_helper file

--- main.py
+++ main.py
@@ -2,56 +2,6 @@ from data_loaders import dailydialog_full
 from preprocessing import text_processing, glove_embeddings, prepare_datasets
 
 import model
-import logging
-import argparse
-
-def _get_logger():
-
-    logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-
-    formatter = logging.Formatter(
-        "[%(asctime)s] " + \
-        "[%(filename)s] " + \
-        "[line:%(lineno)d] " + \
-        "[%(levelname)s] %(message)s"
-    )
-
-    fh = logging.FileHandler('log_file.log', 'w')
-    fh.setFormatter(formatter)
-
-    sh = logging.StreamHandler()
-    sh.setFormatter(formatter)
-
-    logger.addHandler(fh)
-    logger.addHandler(sh)
-
-    return logger
-
-def _create_parser():
-
-    parser = argparse.ArgumentParser(description='Parser for VAT and Dialog Act')
-    
-    parser.add_argument('--model', default='lstm', metavar='model',
-        help='Name of the model for training')
-
-    parser.add_argument('--embed', default='glove', metavar='embed',
-        help='Embeddings required for the model')
-    
-    parser.add_argument('--vat', default=None, metavar='vat',
-        help='Check if the model requires VAT for Semi-supervision')
-
-    return parser.parse_args()
-
-def _get_base_filename(args):
-
-    # File name format: <model>_<embed>_<vat>
-    base_filename = args.model + '_' + args.embed
-
-    if args.vat:
-        base_filename += '_vat'
-    
-    return base_filename
 
 if __name__ == '__main__':
 
