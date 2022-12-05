9c7d9cc36b33229b47b9c0c6129287d9f686c56f
tazeek <tazeek.rakib@gmail.com>
2022-12-06 00:24:38 +0600

    Weights loaded

--- preprocessing/glove_processor.py
+++ preprocessing/glove_processor.py
@@ -1,14 +1,28 @@
-
+import numpy as np
 
 class Glove_Processor:
 
     def __init__(self):
 
-        ...
+        self._path = 'pre_trained\glove.6B.50d.txt'
+        self._weights = self._load_glove_model()
 
-    def _load_glove_model():
+    def _load_glove_model(self):
 
-        ...
+        glove = {}
+
+        with open(self._path, encoding="utf8") as f:
+
+            for line in f.readlines():
+                
+                values = line.split()
+                
+                word = values[0]
+                vector = np.array(values[1:], dtype='float32')
+
+                glove[word] = vector
+            
+        return glove
 
     def create_word_index():
 
@@ -28,4 +42,4 @@ class Glove_Processor:
 
     def custom_collate():
 
-        ....
+        ...
