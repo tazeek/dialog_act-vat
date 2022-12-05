aaf83d419630d60ff784218b4409f35a2f8cf917
tazeek <tazeek.rakib@gmail.com>
2022-12-06 00:28:03 +0600

    Dictionary loader added

--- preprocessing/glove_processor.py
+++ preprocessing/glove_processor.py
@@ -1,3 +1,6 @@
+from data_loaders import dailydialog_full
+from preprocessing import text_processing
+
 import numpy as np
 
 class Glove_Processor:
@@ -7,6 +10,8 @@ class Glove_Processor:
         self._path = 'pre_trained\glove.6B.50d.txt'
         self._weights = self._load_glove_model()
 
+        self._word2idx = self._create_word_index()
+
     def _load_glove_model(self):
 
         glove = {}
@@ -14,19 +19,30 @@ class Glove_Processor:
         with open(self._path, encoding="utf8") as f:
 
             for line in f.readlines():
-                
+
                 values = line.split()
                 
+                # Index 0: Word
+                # Index 1 onwards: Weights
                 word = values[0]
-                vector = np.array(values[1:], dtype='float32')
 
-                glove[word] = vector
+                glove[word] = np.array(values[1:], dtype='float32')
             
         return glove
 
-    def create_word_index():
+    def _create_word_index(self):
 
-        ...
+        # Load the full data of DailyDialog
+        label_loader = dailydialog_full.DailyDialog_Full().fetch_dataframe()
+
+        # Preprocess the text and get tokens for dictionary creation
+        all_possible_words_list = text_processing.preprocess_text(
+            label_loader['utterance'], 
+            remove_punctuation=False
+        )
+
+        # Get word to index dictionary
+        return text_processing.convert_word_index(all_possible_words_list)
 
     def transform_data():
 
