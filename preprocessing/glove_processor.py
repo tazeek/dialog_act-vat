d16357ff5fa414b84354aa1aa19ee9a2e8a38169
tazeek <tazeek.rakib@gmail.com>
2022-12-06 00:33:21 +0600

    Create lookup table

--- preprocessing/glove_processor.py
+++ preprocessing/glove_processor.py
@@ -2,13 +2,14 @@ from data_loaders import dailydialog_full
 from preprocessing import text_processing
 
 import numpy as np
+import torch
 
 class Glove_Processor:
 
     def __init__(self):
 
         self._path = 'pre_trained\glove.6B.50d.txt'
-        self._weights = self._load_glove_model()
+        self._glove_weights = self._load_glove_model()
 
         self._word2idx = self._create_word_index()
 
@@ -46,9 +47,18 @@ class Glove_Processor:
 
     def create_embeddings(self):
 
-        embeddings = np.zeros((len(word_index_dict), 50))
+        embeddings = np.zeros((len(self._word2idx), 50))
 
-        ...
+        count = 0
+        # Loop through word_index_dict
+        for word, index in self._word2idx.items():
+
+            # Check if item is in glove_vectors
+            if word in self._glove_weights:
+                count += 1
+                embeddings[index] = self._glove_weights[word]
+
+        return torch.from_numpy(embeddings).float()
 
     def transform_data():
 
