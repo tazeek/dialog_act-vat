5f1d321805205717778d0a6487bb83236987d49a
tazeek <tazeek.rakib@gmail.com>
2022-12-06 21:15:12 +0600

    BERT preprocessor added

---
+++ preprocessing/bert_processor.py
@@ -0,0 +1,19 @@
+
+
+class Bert_Processor:
+
+    def __init__():
+
+        ...
+
+    def _tokenize_utterance(self):
+
+        ...
+
+    def _perform_transformation(self):
+
+        ...
+
+    def _extract_features(self):
+
+        ...
