3ecdfbdbc7083573faafc107a343d922c8fcdef7
tazeek <tazeek.rakib@gmail.com>
2022-12-07 14:24:33 +0600

    Documentation added

--- preprocessing/bert_processor.py
+++ preprocessing/bert_processor.py
@@ -13,13 +13,23 @@ class Bert_Processor:
         self._model.eval()
 
     def _tokenize_utterance(self):
+        """
+            Convert a sentence to a list of integers
+            PS: Bert Tokenizer returns other information as well
+        """
 
         ...
 
     def _perform_transformation(self):
-
+        """
+            Convert a list of integers to BERT features
+            PS: BERT model returns other pieces of informationa as well
+        """
         ...
 
     def _extract_features(self):
+        """
+            Extract the specific features required for ML
+        """
 
         ...
