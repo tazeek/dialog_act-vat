bcdb1d5fcebf7db1aa77c61cb299dd6761892583
tazeek <tazeek.rakib@gmail.com>
2022-12-07 14:22:54 +0600

    Load tokenizer and model

--- preprocessing/bert_processor.py
+++ preprocessing/bert_processor.py
@@ -1,10 +1,16 @@
-
+from transformers import BertTokenizer, BertModel
 
 class Bert_Processor:
 
     def __init__():
 
         ...
+
+    def _load_model(self):
+
+        self._tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self._model = BertModel.from_pretrained('bert-base-uncased')
+        self._model.eval()
 
     def _tokenize_utterance(self):
 
