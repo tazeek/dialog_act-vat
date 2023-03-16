a1f191563e3d0ee8cac079d29b556d14215df706
tazeek <tazeek.rakib@gmail.com>
2023-03-16 17:55:43 +0800

    Only use input_ids

--- models/bert_finetune.py
+++ models/bert_finetune.py
@@ -5,15 +5,19 @@ class BERT(nn.Module):
     def __init__(self, output_size):
 
         super(BERT, self).__init__()
+
         self.bert_model = BertModel.from_pretrained("bert-base-uncased")
+
+        # Freeze the layers of BERT model
+        for param in self.bert_model.parameters():
+            param.requires_grad = False
+
         self.out = nn.Linear(768, output_size)
         
-    def forward(self,ids,mask,token_type_ids):
+    def forward(self, input_ids):
 
         _ , o2 = self.bert_model(
-            ids,
-            attention_mask=mask,
-            token_type_ids=token_type_ids, 
+            input_ids,
             return_dict=False
         )
         
