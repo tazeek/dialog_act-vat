337ae3d76a3cc6c86457edaf89ffbd964554e09e
tazeek <tazeek.rakib@gmail.com>
2023-03-16 17:59:22 +0800

    BERT is ready

--- models/bert_finetune.py
+++ models/bert_finetune.py
@@ -14,13 +14,19 @@ class BERT(nn.Module):
 
         self.out = nn.Linear(768, output_size)
         
-    def forward(self, input_ids):
+    def forward(self, input):
 
-        _ , o2 = self.bert_model(
-            input_ids,
+        ids = input['input_ids']
+        token_type_ids = input['token_type_ids']
+        attention_mask = input['attention_mask']
+
+        _ , pooled_output  = self.bert_model(
+            input_ids = ids,
+            attention_mask = attention_mask,
+            token_type_ids = token_type_ids,
             return_dict=False
         )
         
-        out= self.out(o2)
+        out= self.out(pooled_output )
         
         return out
